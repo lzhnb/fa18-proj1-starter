bc6cc1625292c81415e4259d1c0f01de979a0510
lzhnb <liang13268932888@outlook.com>
2019-09-25 17:10:14 +0800

    finish part-2

--- lfsr_random.s
+++ lfsr_random.s
@@ -12,9 +12,34 @@ lfsr:
 lfsr_random:
 	la t0 lfsr
 	lhu a0 0(t0)
-
+    
 	# Your Code Here
+    add t1, x0, x0
+    addi t2, x0, 16
+    add t3, a0, x0 # copy array
 
+loop:
+    srli t5, t3, 2   # place 2
+    xor  t4, t3, t5
+    srli t5, t3, 3   # place 3
+    xor  t4, t4, t5
+    srli t5, t3, 5   # place 5
+    xor  t4, t4, t5
+    andi t4, t4, 0x1 # final place
+    slli t4, t4, 15
+    srli t3, t3, 1
+    or   t3, t3, t4
+    addi t1, t1, 1
+	blt  t1, t2, loop
+    
+	# print value
+    # add a1, t3, x0
+    # addi a0, x0, 1 # print int
+    # ecall
+    # addi a1, x0, '\n'
+    # addi a0, x0, 11
+    # ecall
+	addi a0, t3, 0
 	la t0 lfsr
 	sh a0 0(t0)
 	jr ra
--- quad2matrix.s
+++ quad2matrix.s
@@ -9,7 +9,7 @@
 #
 # Recall that quadtree representation uses the following format:
 #     struct qNode {
-#         int leaf;
+#         bool leaf;
 #         int size;
 #         int x;
 #         int y;
@@ -19,5 +19,64 @@
 
 quad2matrix:
 	# Your code here
+	# a0 means root
+	# a1 point to matrix
+	# a2 means map_width
+helper:
+    add t0 a0 x0  # t0 point to quadtree_root(begin)
 
-	jr ra
+find_child:
+    addi sp sp -4 # use stack
+    sw ra 0(sp)   # save ra point
+	lw a3 0(t0)  # get leaf(1) or not(0)
+    beq a3 x0 run_child # if not leaf
+    
+    # is leaf
+    lw t1 4(t0)  # get section_width
+    lw t2 16(t0) # get value
+    # count a4, a5 for x&y
+    lw a4 8(t0)  # get x
+    lw a5 12(t0) # get y
+    add t4 x0 x0 # count_x = 0
+    add t5 x0 x0 # count_y = 0
+    
+	# x=a4, y=a5, section_width=t1, value=t2
+loop_x:
+	beq t4 t1 loop_y # if count_x == section_width: count_y ++
+    add s1 t5 a5     # now_y = count_y + y
+    mul t3 s1 a2     # t3 = now_y * map_width
+    add s2 t4 a4     # now_x = count_x + x
+    add t3 t3 s2     # t3 = now_y * map_width + now_x
+    add s2 a1 t3     # a1 point to matrix head and s2 point to aim
+    sb t2 0(s2)      # use 'sb' to confirm store one byte 'sw' will change 4 bytes
+    addi t4 t4 1
+    j loop_x
+loop_y:
+	add t4 x0 x0
+	addi t5 t5 1   # count_y += 1
+   	beq t5 t1 done # if count_y == section_width lood done
+    j loop_x
+    
+run_child:
+    addi sp sp -4
+    sw t0 0(sp)
+    lw t0 20(t0) # get NW
+    jal find_child
+    lw t0 0(sp)
+    lw t0 24(t0) # get NE
+    jal find_child
+    lw t0 0(sp)
+    lw t0 28(t0) # get SE
+    jal find_child
+    lw t0 0(sp)
+    lw t0 32(t0) # get SW
+    jal find_child
+	lw t0 0(sp)
+    addi sp sp 4
+done:
+	lw ra 0(sp)
+    addi sp sp 4
+    jr ra
+
+
+
